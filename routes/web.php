c5e95477a73cd5594ca6073e49cbaf3571242c60
jadiaelmonteiro <jadiael.monteiro@gmail.com>
2022-12-10 16:43:11 -0300

    :truck: Implementando o kit inicial do bleezer

--- routes/web.php
+++ routes/web.php
@@ -1,61 +1,19 @@
 <?php
-
-use App\Http\Controllers\EpisodesController;
-use App\Http\Controllers\UserController;;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\SeasonController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SeriesController;
-use App\Http\Middleware\Autenticador;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return redirect('/series');
-})->middleware(Autenticador::class);
+    return view('welcome');
+});
 
-Route::resource('/series', SeriesController::class)
-    ->except(['show']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/series/{series}/seasons', [SeasonController::class, 'index'])->name('seasons.index');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
-Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
-
-Route::get('/login', [LoginController::class,  'index'])->name('login');
-Route::post('/login', [LoginController::class, 'store'])->name('singin');
-Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
-
-
-Route::get('/register', [UserController::class, 'create'])->name('user.create');
-Route::post('/register', [UserController::class, 'store'])->name('send');
-
-
-
-
-
-//Route::resource('/series', SeriesController::class)->only(['index, create, store']);
-// Route::controller(SeriesController::class)->group(function() {
-
-//     Route::get('/series/index', 'index')->name('series.index');
-//     Route::get('/series/create', 'create')->name('series.create');
-//     Route::post('/series/store', 'store')->name('series.store');
-//     Route::get('/series/editar/{id}', 'edit')->name('series.editar');
-//     Route::put('/series/update', 'update')->name('series.update');
-//     //Route::get('/series/deletar/{id}', 'destroy');
-//     Route::delete('/series/deletar/{id}', 'destroy')->name('series.destroy');
-
-    
-// });
-
-//Usando nomeclaturas da documentação das funcoes
-//Route::resource('/series', SeriesController::class);
+require __DIR__.'/auth.php';
